afd62329e25d5e83050490cab19148af7fa81f11
yekai <www.yekai.com@foxmail.com>
2019-11-19 14:22:13 +0800

    这是第二次的提交

--- git-test01/src/TestDemo1.java
+++ git-test01/src/TestDemo1.java
@@ -3,4 +3,9 @@ public class TestDemo1 {
         System.out.println("1111");
         System.out.println("改变");
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
