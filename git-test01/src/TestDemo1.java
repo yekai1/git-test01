8c5be688a00de6ab123303a1b87dff24c93deb68
yekai <www.yekai.com@foxmail.com>
2019-11-19 14:27:46 +0800

    这是第二次的提交

--- git-test01/src/TestDemo1.java
+++ git-test01/src/TestDemo1.java
@@ -8,4 +8,14 @@ public class TestDemo1 {
     public String toString() {
         return super.toString();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
